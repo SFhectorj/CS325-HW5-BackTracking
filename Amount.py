68fb343cf0368a202b2ae7534a7ad98ae4f8a326
github_username <github_email>
2024-11-05 19:56:59 -0800

    homework 5

--- Amount.py
+++ Amount.py
@@ -1,6 +1,14 @@
 def amount(A, S):
+    '''
+    
+    '''
     # Part 1: Sorting
     A.sort()
     result_list = []
+
+    def combination_helper(remaining_sum, combo_pair, starting_point):
+
+
+
 
 
